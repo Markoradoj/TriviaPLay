6fcc5f5cdd571ff95924c2d5dd31fd8a99de1f22
Markoradoj <G00436648@atu.ie>
2025-01-05 19:01:14 +0000

    Added categorytype list

--- TriviaPLay/StartMenu.xaml.cs
+++ TriviaPLay/StartMenu.xaml.cs
@@ -16,36 +16,36 @@ public partial class StartMenu : ContentPage
 
 	};
 
-	//get a list of valid options for the category of API
-	Dictionary<short, string> categoryType = new Dictionary<short, string>()
-	{
-		{0, "Mixed" },
-		{9, "General Knowledge"},
-		{10, "Books"},
-		{11, "Film" },
-		{12, "Music" },
-		{13, "Musicals & Theatres"},
-		{14, "Televsion" },
-		{15, "Video Games" },
-		{16, "Board Games" },
-		{17,  "Nature" },
-		{18, "Computers" },
-		{19, "Mathematics" },
-		{20, "Mythology" },
-		{21, "Sports" },
-		{22, "Geography" },
-		{23, "History" },
-		{24, "Politics" },
-		{25, "Art" },
-		{26, "Celebrities" },
-		{27, "Animals" },
-		{28, "Vehicles" },
-		{29, "Comics" },
-		{30, "Gadgets" },
-		{31, "Anime & Manga" },
-		{32, "Cartoons & Animations" }
+    //get a list of valid options for the category of API
+    Dictionary<short, string> categoryType = new Dictionary<short, string>()
+    {
+        {0, "Mixed" },
+        {9, "General Knowledge"},
+        {10, "Books"},
+        {11, "Film" },
+        {12, "Music" },
+        {13, "Musicals & Theatres"},
+        {14, "Televsion" },
+        {15, "Video Games" },
+        {16, "Board Games" },
+        {17,  "Nature" },
+        {18, "Computers" },
+        {19, "Mathematics" },
+        {20, "Mythology" },
+        {21, "Sports" },
+        {22, "Geography" },
+        {23, "History" },
+        {24, "Politics" },
+        {25, "Art" },
+        {26, "Celebrities" },
+        {27, "Animals" },
+        {28, "Vehicles" },
+        {29, "Comics" },
+        {30, "Gadgets" },
+        {31, "Anime & Manga" },
+        {32, "Cartoons & Animations" }
 
-	};
+    };
 
 	public StartMenu()
 	{
