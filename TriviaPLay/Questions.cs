c88058c11d7d45c37a2301bd6e4bdedd8948a1c1
Markoradoj <G00436648@atu.ie>
2025-01-06 17:10:48 +0000

    Added questioms.cs page

---
+++ TriviaPLay/Questions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TriviaPLay
+{
+    [Serializable]
+    public class Questions
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+
+    }
+}
