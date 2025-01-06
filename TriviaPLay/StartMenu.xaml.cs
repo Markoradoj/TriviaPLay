cc8fad48154f3496d07053459728b0b50e8a6f17
Markoradoj <G00436648@atu.ie>
2025-01-06 18:45:07 +0000

    Added category loading

--- TriviaPLay/StartMenu.xaml.cs
+++ TriviaPLay/StartMenu.xaml.cs
@@ -1,71 +1,48 @@
 
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Nodes;
 
 namespace TriviaPLay;
 
 public partial class StartMenu : ContentPage
 {
+	private List<Question> _questions;
+	private Dictionary<string, string> _categories;
 
 
-    string[] diffuclty = { "Mix", "Easy", "Medium", "Hard" };
-
-    //to recognize the key is loaded properly within the api
-    Dictionary<short, string> responses = new Dictionary<short, string>()
-    {
-        {0, "Success" },
-        {1, "No Results" },
-        {2, "Invalid Paremeter" },
-        {3, "Token not Found" },
-        {4, "Token session empty" }
-
-
-    };
-
-    //get a list of valid options for the category of API
-    Dictionary<short, string> categoryType = new Dictionary<short, string>()
-	{
-		{0, "Mixed" },
-		{9, "General Knowledge"},
-		{10, "Books"},
-		{11, "Film" },
-		{12, "Music" },
-		{13, "Musicals & Theatres"},
-		{14, "Televsion" },
-		{15, "Video Games" },
-		{16, "Board Games" },
-		{17, "Nature" },
-		{18, "Computers" },
-		{19, "Mathematics" },
-		{20, "Mythology" },
-		{21, "Sports" },
-		{22, "Geography" },
-		{23, "History" },
-		{24, "Politics" },
-		{25, "Art" },
-		{26, "Celebrities" },
-		{27, "Animals" },
-		{28, "Vehicles" },
-		{29, "Comics" },
-		{30, "Gadgets" },
-		{31, "Anime & Manga" },
-		{32, "Cartoons & Animations" }
-
-	};
-
-	public StartMenu()
+    public StartMenu()
 	{
 		InitializeComponent();
 
-		//picker setups
-		selCategory.ItemsSource = categoryType.Keys.ToList();
-		selDifficulty.ItemsSource = diffuclty;
-
-		selCategory.SelectedIndex = 0;
-		selDifficulty.SelectedIndex = 0;
-
 	}
 
+	private async void LoadCatagory()
+	{
+		try
+		{
+			using var client = new HttpClient();
+			string response = await client.GetStringAsync("https://opentdb.com/api_category.php");
+			var categoryResponse = JsonConvert.DeserializeObject<CategoryResponse>(response);
+
+			_categories = categoryResponse.TriviaCategories.ToDictionary(c => c.Name, c => c.Id.ToString());
+
+			foreach (var category in _categories.Keys)
+			{
+				selCategory.Items.Add(category);
+			}
+		}
+
+		catch (Exception ex)
+		{
+			await DisplayAlert("Notice", $"Failed to load catagories: {ex.Message}", "OK");
+		}
+	}
     private void Button_Clicked(object sender, EventArgs e)
     {
 
     }
+
+
 }
