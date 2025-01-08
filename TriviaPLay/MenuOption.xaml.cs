252922172bbff9ef40d77597b3998911f2b7344e
Markoradoj <G00436648@atu.ie>
2025-01-08 17:08:00 +0000

    Added global button colors and exit button

--- TriviaPLay/MenuOption.xaml.cs
+++ TriviaPLay/MenuOption.xaml.cs
@@ -6,4 +6,25 @@ public partial class MenuOption : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void SetBlueButtonClicked(object sender , EventArgs e)
+	{
+		Application.Current.Resources["ButtonBackgroundColor"] = Colors.Blue;
+	}
+
+    private void SetRedButtonClicked(object sender, EventArgs e)
+    {
+        Application.Current.Resources["ButtonBackgroundColor"] = Colors.Red;
+    }
+
+    private void SetGreenButtonClicked(object sender, EventArgs e)
+    {
+        Application.Current.Resources["ButtonBackgroundColor"] = Colors.Green;
+    }
+
+
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopToRootAsync();
+    }
 }
